eb12b213379b82f6cae8e3852df4f23588f1326b
Maeva <maev.leca@gmail.com>
2020-06-03 16:54:36 +0900

    end document-term matrix, more cleaning

--- analysis.py
+++ analysis.py
@@ -12,24 +12,15 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 
-#define the stop words list with more words, according to special case
-def updateStopWord(list):
-    toAdd = ['years','year']
-    for elem in toAdd:
-        list.append(elem)
-    return list
-
-stop = updateStopWord(stopwords.words('english'))
-
-
-
 def main():
+    global dicManSyn
     db = getDB("database.txt")
     dicManSyn = mangaSynopsis(db)
-    getDTMatrix(dicManSyn)
+    dtm = getDTMatrix(dicManSyn)
+    print(dtm)
 
 
-
+#get content from file made by kitsuapi
 def getDB(file):
     f = open(file, "r")
     content = f.read()
@@ -37,7 +28,7 @@ def getDB(file):
     db = stringToList(content)
     return db
 
-
+#transform content file to list, for easier manipulation
 def stringToList(str):
     db = []
     mangas = str.split("{")
@@ -55,13 +46,15 @@ def stringToList(str):
         db.append(x)
     return db
 
-
+#get a dict with key: title, value: synopsis
 def mangaSynopsis(database):
     data = {}
     for mangas in database:
         data[mangas['title']] = mangas['synopsis']
     return data
 
+
+#********************** get Document-Term matrix **************************#
 
 def getDTMatrix(dict):
     names = getIndex(dict)
@@ -70,14 +63,60 @@ def getDTMatrix(dict):
     vec = CountVectorizer()
     X = vec.fit_transform(dataCleaned)
     df = pd.DataFrame(X.toarray(), index=names, columns=vec.get_feature_names())
-    print(df)
+    return df
 
 
+    ##### parameters of the matrix #####
+#return an array with the title of each manga
+def getIndex(dict):
+    res = []
+    for key in dict:
+        res.append(key)
+    return res
+
+
+#return an array with the synopsis of each mangas
+def getData(dict):
+    res = []
+    for datas in dict.values():
+        res.append(datas)
+    return res
+
+    #############################
+
+    ##### clean data for the matrix #####
+def cleanData(list):
+    global stop
+    clean = []
+    allWords = []
+    i = 1
+    nb_round = 2
+    while i <= nb_round:
+        for datas in list:
+            data = removePonct(datas)
+            str = ""
+            tokens = word_tokenize(data)
+            filtered = [w for w in tokens if not w in stop]
+            allWords.append(filtered)
+            if i == nb_round: #add words only at the final iteration, to have clean result.
+                for words in filtered:
+                    if removeDigit(words):
+                        str += words+" "
+                clean.append(str)
+        if i < nb_round: #update stopWord for each iteration except the last one
+            removeCommonWords(allWords)
+        i += 1
+    return clean
+
+
+
+        # remove ponctuation
 def removePonct(str):
-    exclude = ['!','"','#','%','$','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','\\','^','_','`','{','}','|','~']
+    exclude = ['!','"','#','%','$','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','\\','^','_','`','{','}','|','~','\\n']
     res = ''.join(char for char in str if char not in exclude)
     return res
 
+        # remove digit
 def removeDigit(str):
     pattern = '^([A-Za-z]|-)+$'
     regex = re.compile(pattern)
@@ -86,34 +125,44 @@ def removeDigit(str):
     else:
         return ""
 
+        ###########################
 
-def cleanData(list):
+#******************** end get Document-Term Matrix  **********************#
+
+#******************* clean data ******************************************#
+
+def removeCommonWords(listOfList):
+    global dicManSyn
     global stop
-    clean = []
-    for datas in list:
-        data = removePonct(datas)
-        str = ""
-        tokens = word_tokenize(data)
-        filtered = [w for w in tokens if not w in stop]
-        for words in filtered:
-            if removeDigit(words):
-                str += words+" "
-        clean.append(str)
-    return clean
+    global toAdd
+    tmp_result = {} #key: word, value: number of document where they appear
+    for listOfWords in listOfList:
+        for word in listOfWords:
+            tmp_result[word] = 0
+            for synopsis in dicManSyn.values():
+                if word in synopsis:
+                    tmp_result[word] += 1
+    for k, v in tmp_result.items():
+        if (v >= len(listOfList)/2):
+            toAdd.append(k)
+    stop = updateStopWord(stop,toAdd)
+
+#******************* end clean data **************************************#
 
 
-def getIndex(dict):
-    res = []
-    for key in dict:
-        res.append(key)
-    return res
+#********************* init and global variables *************************#
 
-def getData(dict):
-    res = []
-    for datas in dict.values():
-        res.append(datas)
-    return res
+toAdd = ["year", "years"]
+#define the stop words list with more words, according to special case
+def updateStopWord(listStopWords, listToAdd):
+    for elem in listToAdd:
+        if elem not in listStopWords:
+            listStopWords.append(elem)
+    return listStopWords
 
+stop = updateStopWord(stopwords.words('english'), toAdd )
+dicManSyn = {}
+#************************************************************************#
 
 if __name__ == "__main__":
 	main()
